cce826d606621fe8b2bf6368613f734a2354c1b1
Jan Vlnas <jan.vlnas@superface.ai>
2022-05-06 11:12:58 +0200

    Implement publish-post

--- scripts/publish-post.js
+++ scripts/publish-post.js
@@ -1,25 +1,39 @@
 const { inspect } = require('node:util');
 const { SuperfaceClient } = require('@superfaceai/one-sdk');
 const { withAccessToken } = require('../utils/tokens-utils');
+const fs = require('fs/promises');
 
-const publishPost = async (message) => {
-  const sdk = new SuperfaceClient();
+const provider = 'pinterest';
+const sdk = new SuperfaceClient();
 
+const publishPost = async (profileId, fileOrUrl) => {
+  const media = {
+    url: undefined,
+    contents: undefined,
+    altText: 'Some alt text',
+  };
+
+  if (fileOrUrl.startsWith('http')) {
+    media.url = fileOrUrl;
+  } else {
+    media.contents = await fs.readFile(fileOrUrl);
+  }
+
+  const input = {
+    profileId,
+    text: 'Some description',
+    link: 'https://example.com',
+    media: [media],
+  };
   try {
-    const provider = await sdk.getProvider('twitter');
     const profile = await sdk.getProfile('social-media/publish-post');
     const result = await withAccessToken((accessToken) =>
-      profile.getUseCase('PublishPost').perform(
-        {
-          text: message,
+      profile.getUseCase('PublishPost').perform(input, {
+        provider,
+        parameters: {
+          accessToken,
         },
-        {
-          provider,
-          parameters: {
-            accessToken,
-          },
-        }
-      )
+      })
     );
     console.log(inspect(result.unwrap(), false, Infinity, true));
   } catch (err) {
@@ -27,6 +41,7 @@ const publishPost = async (message) => {
   }
 };
 
-const message = process.argv[2] || 'Hello from Superface.';
+const profileId = process.argv[2];
+const fileOrUrl = process.argv[3] || 'https://placekitten.com/200/300';
 
-publishPost(message);
+publishPost(profileId, fileOrUrl);
